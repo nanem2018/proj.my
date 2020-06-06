3b56284c904f04ff3dbf8bea9b00196299a8533a
[nanem2018] <[nanemchaninova@gmail.com]>
2020-06-06 23:23:07 +0300

    добавлена функция хэш. пароля перед отправкой в бд

--- public/registration_controler.php
+++ public/registration_controler.php
@@ -6,11 +6,15 @@ autoload('../engine', 1, ['autoload.php']);
 <?
 $user_name = trim(strip_tags($_POST['first_name']));
 $user_login = trim(strip_tags($_POST['display_name']));
-$user_password = trim(strip_tags($_POST['reg_pass']));
+$user_pass = $_POST['reg_pass'];
 $user_email = trim(strip_tags($_POST['reg_email']));
 
+
+//хэшируем пароль
+$user_password=password_hash($user_pass, PASSWORD_BCRYPT);
+
 if ($user_name && $user_login && $user_password && $user_email) {
-	$insert_query = sprintf("INSERT INTO `users` (`user_name`, `user_login`, `user_password`, `user_email`)  VALUES (\"%s\", \"%s\",\"%u\",\"%s\");", $user_name, $user_login, $user_password, $user_email);
+	$insert_query = sprintf("INSERT INTO `users` (`user_name`, `user_login`, `user_password`, `user_email`)  VALUES (\"%s\", \"%s\",\"%s\",\"%s\");", $user_name, $user_login, $user_password, $user_email);
 	executeQuery($insert_query);
 }
 
